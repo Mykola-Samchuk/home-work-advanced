3f3c71dbc57066aa849c9e77125cc6a0d3c03fab
Kolya Samchuk <mykola.samchuk@gmail.com>
2021-07-09 16:30:05 +0300

    add audio and Event fot key

--- hw-10-event/main.js
+++ hw-10-event/main.js
@@ -6,6 +6,25 @@ document.addEventListener(`keydown`, function (event){
     audioPlay.currentTime = 0;
     audioPlay.play()
     listElem.classList.add('playing');
+    
+    
 })
 
+document.addEventListener(`keyup`, function (event){
+    const listElem = document.querySelector(`.list-elem[data-key="${event.code}"]`)
+    listElem.classList.remove('playing');
+    
+})
 
+const list = document.querySelector(`.list`)
+list.addEventListener(`click`, function(event){
+    const listEven = event.target.getAttribute(`data-key`)
+   const audio = document.querySelectorAll(`.audio-road`)
+    
+    
+    for(let i of audio ){      
+        i.getAttribute(`data-key`)
+    }
+    
+    
+})
