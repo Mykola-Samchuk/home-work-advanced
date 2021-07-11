6e5bb0e92b3c5343e1c5444aff1b200a204c4bbf
Kolya Samchuk <mykola.samchuk@gmail.com>
2021-07-11 23:22:12 +0300

    add click event

--- hw-10-event/main.js
+++ hw-10-event/main.js
@@ -1,30 +1,24 @@
 document.addEventListener(`keydown`, function (event){
-    const listElem = document.querySelector(`.list-elem[data-key="${event.code}"]`)
-    const audioPlay = document.querySelector(`.audio-road[data-key="${event.code}"]`)
+    const listElem = document.querySelector(`.list-elem[data-key="${event.code}"]`);
+    const audioPlay = document.querySelector(`.audio-road[data-key="${event.code}"]`);
     
     if(!audioPlay) return
     audioPlay.currentTime = 0;
-    audioPlay.play()
+    audioPlay.play();
     listElem.classList.add('playing');
-    
-    
 })
 
 document.addEventListener(`keyup`, function (event){
-    const listElem = document.querySelector(`.list-elem[data-key="${event.code}"]`)
+    const listElem = document.querySelector(`.list-elem[data-key="${event.code}"]`);
     listElem.classList.remove('playing');
-    
 })
 
-const list = document.querySelector(`.list`)
-list.addEventListener(`click`, function(event){
-    const listEven = event.target.getAttribute(`data-key`)
-   const audio = document.querySelectorAll(`.audio-road`)
-    
-    
-    for(let i of audio ){      
-        i.getAttribute(`data-key`)
-    }
-    
-    
-})
+const list = document.querySelectorAll(`.list-elem`);
+
+list.forEach((item)=>{
+    item.addEventListener(`click`, function(event){
+        const audio = document.querySelector(`.audio-road[data-key="${item.dataset.key}"]`);
+        audio.play();
+        audio.currentTime = 0;
+    });
+});
