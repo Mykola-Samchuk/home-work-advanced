b5da2316bd06c9fc254ed913a3f445e1ff6d2eb7
Kolya Samchuk <mykola.samchuk@gmail.com>
2021-07-17 16:38:10 +0300

    add hw-11-promises

---
+++ hw-11-promises/main.js
@@ -0,0 +1,23 @@
+async function getRandomChinese(length){
+
+    let res = ``;
+
+    for(i = 0; i < length; i++){
+      let getPromise = new Promise((resolve, reject)=>{
+          
+        setTimeout(() => {
+             let sign = Date.now().toString().slice(-5);
+             let signString = String.fromCharCode(sign);
+
+             resolve(signString);
+          }, 50*length);
+      })
+      res += await getPromise;
+      
+    }
+    
+    console.log(res)
+}
+
+
+getRandomChinese(4)
