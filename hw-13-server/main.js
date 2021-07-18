50f1d7620d2173e5e42af0063cc91d47649f3706
Kolya Samchuk <mykola.samchuk@gmail.com>
2021-07-18 15:13:38 +0300

    add array at name

--- hw-13-server/main.js
+++ hw-13-server/main.js
@@ -1,12 +1,24 @@
 const users = document.getElementById(`users-list`)
+const baseUrl = `https://swapi.dev/api/films/2/`
+function createTemplate(){
+    return `
+    <div class="character-item">
+        <h2 class="character-name"></h2>
+        <span class="character-birthday"></span>
+        <span class="sex"></span>
+    </div>
+    `
+}
 
-function getUsers(){
-    fetch(`https://swapi.dev/api/films/2/`)
+fetch(baseUrl)
     .then(function(response){
         return response.json()
     })
     .then(function(data){
-        console.log(data);
+            data.characters.forEach((item)=>{
+                console.log(item)
+            })
+    
     })
-}
-getUsers()
+    
+    
