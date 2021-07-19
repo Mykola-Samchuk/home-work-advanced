385c4fc673b4e938c3dac0a8e59126aa98646091
Kolya Samchuk <mykola.samchuk@gmail.com>
2021-07-19 23:36:13 +0300

    add palnets

--- hw-12-server/main.js
+++ hw-12-server/main.js
@@ -1,12 +1,18 @@
 const users = document.getElementById(`users-list`);
-const baseUrl = `https://swapi.dev/api/films/2/`;
-let list = document.querySelector(`.character-item`);
+const planets = document.getElementById(`planet-list`);
+const userUrl = `https://swapi.dev/api/films/2/`;
+const planetsUrl = `https://swapi.dev/api/planets`;
+
+let listUser = document.querySelector(`.character-item`);
+let listPlanet = document.querySelector(`.planet-item`);
 
 users.addEventListener(`click`, getUsers);
+planets.addEventListener(`click`, getPlanets);
+
 
 async function getUsers(){
 
-    const response = await fetch(baseUrl);
+    const response = await fetch(userUrl);
     const content = await response.json();
     const users = content.characters;
 
@@ -14,13 +20,31 @@ async function getUsers(){
 
         let response = await fetch(character);
         character = await response.json();
-        list.innerHTML+=`
+        listUser.innerHTML+=`
         <div class="user">
         <h2 class="character-name">${character.name}</h2>
         <span class="character-birthday">${character.birth_year}</span>
         <span class="sex">${character.gender}</span>
         </div>
         `;
+    };
+    
+};
+
+async function getPlanets(){
+
+    const response = await fetch(planetsUrl);
+    const content = await response.json();
+    const planets = await content.results;
+
+    for(let planet of planets){
+
+        listPlanet.innerHTML += `
+        <div class="planet">
+            <h2 class="planet-name">${planet.name}</h2>
+        </div>
+        `
+        
     };
     
 };
