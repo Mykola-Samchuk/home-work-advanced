a48c16a215db49751396d628f81ce0f3dab72b33
Kolya Samchuk <mykola.samchuk@gmail.com>
2021-07-20 22:33:20 +0300

    add event for click planets and planets hide

--- hw-12-server/main.js
+++ hw-12-server/main.js
@@ -1,13 +1,34 @@
 const users = document.getElementById(`users-list`);
 const planets = document.getElementById(`planet-list`);
+const planetsNext = document.getElementById( `planet-next`);
+const planetsPrevious = document.getElementById( `planet-previous`);
 const userUrl = `https://swapi.dev/api/films/2/`;
-const planetsUrl = `https://swapi.dev/api/planets`;
+const planetsUrl = `https://swapi.dev/api/planets/?page=`;
 
+let getPage = 1;
 let listUser = document.querySelector(`.character-item`);
 let listPlanet = document.querySelector(`.planet-item`);
 
+
 users.addEventListener(`click`, getUsers);
-planets.addEventListener(`click`, getPlanets);
+planets.addEventListener(`click`, ()=>{getPlanets(planetsUrl)});
+
+planetsNext.addEventListener(`click`, function getNext(){
+    if(getPage <6){
+        listPlanet.innerHTML =``
+        getPage += 1
+        getPlanets(planetsUrl + getPage)
+    };
+});
+
+planetsPrevious.addEventListener(`click`, function getPrev(){
+    if(getPage <= 6){
+        listPlanet.innerHTML =``
+        getPage -= 1
+        getPlanets(planetsUrl + getPage)
+    };
+});
+
 
 
 async function getUsers(){
@@ -20,6 +41,7 @@ async function getUsers(){
 
         let response = await fetch(character);
         character = await response.json();
+
         listUser.innerHTML+=`
         <div class="user">
         <h2 class="character-name">${character.name}</h2>
@@ -31,7 +53,7 @@ async function getUsers(){
     
 };
 
-async function getPlanets(){
+async function getPlanets(planetsUrl){
 
     const response = await fetch(planetsUrl);
     const content = await response.json();
@@ -43,10 +65,13 @@ async function getPlanets(){
         <div class="planet">
             <h2 class="planet-name">${planet.name}</h2>
         </div>
-        `
-        
+        `;
     };
+
+    document.querySelector(`.btn-planet`).style.display=`flex`;
     
 };
+
+
 
 
