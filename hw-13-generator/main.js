931412929d163b0e6a809cf4cf0e7b8738cff6cc
Kolya Samchuk <mykola.samchuk@gmail.com>
2021-07-25 20:26:21 +0300

    add style.css

--- hw-13-generator/main.js
+++ hw-13-generator/main.js
@@ -1,11 +1,11 @@
 function* createIdGenerator() {
     let id = 1;
     for(let i = 1; i++;){
-        yield id
-        id++
-    }
+        yield id;
+        id++;
+    };
     
-  }
+  };
   const idGenerator = createIdGenerator();
 
 console.log(idGenerator.next().value);
@@ -14,22 +14,34 @@ console.log(idGenerator.next().value);
 console.log(idGenerator.next().value);
 console.log(idGenerator.next().value);
 
+
 function* newFontGenerator(number){
-    const size = 2
+    const size = 2;
     
     while(true){
-      let value = yield + number
-      if(value ===`up`)
-        yield number += size
+      let value = yield number;
 
-       
-      else if(value ===`down`)
-         number -= size
-       
-    }
+      if(value ===`up`){
+         number +=  size;
+      }
+      else if(value ===`down`){
+         number -= size;
+        
+      };
+      
+    };
     
-    
+};
 
-}
+const fontGenerator = newFontGenerator(32);
+const h1 = document.querySelector(`.cnsl`);
+const up = document.querySelector(`.up`);
+const down = document.querySelector(`.down`);
 
-const fontGenerator = newFontGenerator()
+up.addEventListener(`click`, ()=>{
+  h1.style.fontSize = `${fontGenerator.next(`up`).value}px`
+});
+
+down.addEventListener(`click`, ()=>{
+  h1.style.fontSize = `${fontGenerator.next(`down`).value}px`
+});
